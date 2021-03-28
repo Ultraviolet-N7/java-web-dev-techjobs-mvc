40a56534ba64b86ca3792fb11be7278ada7a4a63
Ultraviolet-N7 <meieramberj@gmail.com>
2021-03-28 13:57:36 -0500

    added displaySearchResults handler to Search Controller

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
@@ -23,6 +23,19 @@ public class SearchController {
         return "search";
     }
 
+    @RequestMapping(value = "results")
+    public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
+        ArrayList<Job> jobs;
+        if (searchTerm.toLowerCase().equals("all") || searchTerm.equals("")) {
+            jobs = JobData.findAll();
+        } else {
+            jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+        }
+        model.addAttribute("jobs", jobs);
+        model.addAttribute("columns", columnChoices);
+            return "list-jobs";
+    }
+
     // TODO #3 - Create a handler to process a search request and render the updated search view.
 
 }
